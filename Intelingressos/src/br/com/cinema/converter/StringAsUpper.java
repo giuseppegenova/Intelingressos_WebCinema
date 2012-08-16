818e498a8244497bc2b7cc06fe1f2f03251be7b1
lgenova <systemasjava@gmail.com>
2012-08-16 14:18:39 -0400

    Atualização do primefaces e inserção de classe

---
+++ Intelingressos/src/br/com/cinema/converter/StringAsUpper.java
@@ -0,0 +1,22 @@
+package br.com.cinema.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+/*@Name("convertToUpper")
+@org.jboss.seam.annotations.faces.Converter
+@BypassInterceptors*/
+public class StringAsUpper implements Converter {
+
+ public Object getAsObject(FacesContext facesContext
+                           , UIComponent component, String value) {
+  return value.toString().toUpperCase();
+ }
+
+ public String getAsString(FacesContext facesContext
+                          , UIComponent component, Object value) {
+  return value.toString().toUpperCase();
+ }
+
+}
