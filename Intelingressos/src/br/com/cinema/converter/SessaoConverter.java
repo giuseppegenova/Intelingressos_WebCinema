e78ba91aa61d7e3375be86251d04b990203b1a7b
lgenova <systemasjava@gmail.com>
2012-08-30 10:42:43 -0400

    atualizaçoes

---
+++ Intelingressos/src/br/com/cinema/converter/SessaoConverter.java
@@ -0,0 +1,68 @@
+package br.com.cinema.converter;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+import br.com.cinema.entity.Sessao;
+import br.com.cinema.facade.local.SessaoFacadeLocal;
+
+@FacesConverter(value="sessaoConverter")
+@Stateless
+public class SessaoConverter implements Converter {
+	
+	private Sessao sessao;
+	
+	public Sessao getSessao() {
+		return sessao;
+	}
+
+	public void setSessao(Sessao sessao) {
+		this.sessao = sessao;
+	}
+
+	public SessaoConverter() {
+		sessao = new Sessao();
+	}
+
+	@EJB
+	private SessaoFacadeLocal sessaoFacade;
+	  
+    public Object getAsObject(FacesContext facesContext, UIComponent uicomp, String value) {  
+    	
+    	long sessaoId;
+    	
+		try {		
+			sessaoId = Long.parseLong(value);
+			return sessaoFacade.find(sessaoId);
+		} catch (NumberFormatException exception) {
+			throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro de converção", "Erro ao selecionar este item"));
+		}
+
+		
+    }  
+  
+    public String getAsString(FacesContext facesContext, UIComponent uicomp, Object value) {  
+    	
+    	try {
+    		
+    		if (value == null || value.equals("")) {
+    			return "";
+    		}	
+    		
+    		sessao = (Sessao) value;
+    		
+    		if (sessao.getId() == 0){
+    			return "";
+    		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return String.valueOf(sessao.getId());
+    }    
+}
