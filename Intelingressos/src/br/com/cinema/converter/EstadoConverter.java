e2c0ec363a345de9499b564bab7aab9705d06951
Giuseppe <systemasjava@gmail.com>
2012-09-12 00:46:23 -0300

    Remoção de Converters invalidos e atualização do converter para estado

--- Intelingressos/src/br/com/cinema/converter/EstadoConverter.java
+++ Intelingressos/src/br/com/cinema/converter/EstadoConverter.java
@@ -1,69 +1,27 @@
 package br.com.cinema.converter;
 
-import br.com.cinema.entity.Estado;
-import br.com.cinema.facade.local.EstadoFacadeLocal;
-import javax.ejb.EJB;
+import br.com.cinema.managedbean.ClienteMB;
 import javax.ejb.Stateless;
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
 @FacesConverter(value="estadoConverter")
 @Stateless
 public class EstadoConverter implements Converter {
 	
-	private Estado estado;
-	
-	public Estado getEstado() {
-		return estado;
-	}
-
-	public void setEstado(Estado estado) {
-		this.estado = estado;
-	}
-
-	public EstadoConverter() {
-		estado = new Estado();
-	}
-
-	@EJB
-	private EstadoFacadeLocal estadoFacade;
-	  
+@Override
+    public Object getAsObject(FacesContext arg0, UIComponent arg1, String key) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ClienteMB clienteMB = (ClienteMB) context.getELContext().getELResolver().getValue(context.getELContext(), null, "clienteMB");
+ 
+        return null;//clienteMB.getClienteByName(key);
+    }
+ 
     @Override
-    public Object getAsObject(FacesContext facesContext, UIComponent uicomp, String value) {  
-    	
-    	long estadoId;
-    	
-		try {		
-			estadoId = Long.parseLong(value);
-			return estadoFacade.find(estadoId);
-		} catch (NumberFormatException exception) {
-			throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro de conver��o", "Erro ao selecionar este item"));
-		}
-
-		
-    }  
-  
-    @Override
-    public String getAsString(FacesContext facesContext, UIComponent uicomp, Object value) {  
-    	
-    	try {
-    		
-    		if (value == null || value.equals("")) {
-    			return "";
-    		}	
-    		
-    		estado = (Estado) value;
-    		
-    		if (estado.getId() == 0){
-    			return "";
-    		}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-    	return String.valueOf(estado.getId());
-    }    
+    public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+        return arg2.toString();
+    }
+    
 }
